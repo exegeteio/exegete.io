85e255ed4fc92fd8f07f725f287951b8325ffed8
Jack M <exegete@mac.com>
2022-12-21 06:53:01 -0700

    Updates frontend header/foooter/css/font

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,7 +1,7 @@
 import "index.css"
+import "simple.css"
+import "font.css"
 import "syntax-highlighting.css"
 
 // Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
-
-console.info("Bridgetown is loaded!")
